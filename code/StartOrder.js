311906ce214eae27204f3eed3076857ee3ed9a5e
earthworm925 <earthworm925@gmail.com>
2019-12-06 21:41:08 +0900

    for quantity prompt

--- code/StartOrder.js
+++ code/StartOrder.js
@@ -1,8 +1,12 @@
 var console = require('console')
+
 module.exports.function = function startOrder (userRequest) {
   console.log(userRequest);
+
   if (userRequest && !userRequest.quantity) {
     userRequest.quantity = 1;
+    // 디폴트 값으로 정해도 되고, BuildUserRequest에서 인풋 Quantity 컨셉을 Required로
+    // 해서 항상 되묻도록 해도됩니다.
   }
   var order = {
     userRequests : [userRequest]
