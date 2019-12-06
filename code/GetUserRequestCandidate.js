a7dffd60be3d2576960939eab65047747a18c1c9
earthworm925 <earthworm925@gmail.com>
2019-12-06 20:54:50 +0900

    add code

---
+++ code/GetUserRequestCandidate.js
@@ -0,0 +1,8 @@
+module.exports.function = function getUserRequestCandidate () {
+  return [
+    {productName : '사과'},
+    {productName : '수박'},
+    {productName : '딸기'},
+    {productName : '배'},
+  ]
+}
--- code/StartOrder.js
+++ code/StartOrder.js
@@ -1,9 +1,11 @@
 var console = require('console')
 module.exports.function = function startOrder (userRequest) {
   console.log(userRequest);
+  if (userRequest && !userRequest.quantity) {
+    userRequest.quantity = 1;
+  }
   var order = {
     userRequests : [userRequest]
   }
-  console.log(order);
   return order;
 }
